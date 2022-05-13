f8fcfb143bd29fec02b0663d8381068d38fd1010
mininpark <parkmini.93@gmail.com>
2022-05-14 01:17:14 +0200

    503 error 1

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000;
 
 
 app.use(express.static(path.join(__dirname, 'public')))
-app.get('/', (req, res, next) => {
+app.get('', (req, res, next) => {
     res.sendFile(__dirname + '/index.html')
 });
 
